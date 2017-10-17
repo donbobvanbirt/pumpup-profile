238130f00e911e7e5d9fead1a3cf6ab53b0220b1
Donovan <donovan6925@gmail.com>
2017-10-16 21:41:39 -0400

    added more tests

---
+++ src/components/Header/header.spec.js
@@ -0,0 +1,29 @@
+/*global shallow should*/
+/*eslint no-undef: ["error", { "typeof": true }] */
+
+import React from 'react'
+
+import Header from './index'
+import Bio from './Bio'
+
+const noop = () => {}
+
+describe('Header', () => {
+  const defaultProps = {
+    bio          : '',
+    name         : '',
+    profileImage : '',
+    toggleBio    : noop,
+    truncateBio  : true,
+  }
+
+  const wrapper = shallow(<Header {...defaultProps} />)
+
+  it('successfully renders', () => {
+    should.equal(wrapper.length, 1)
+  })
+
+  it('renders Bio component', () => {
+    should.equal(wrapper.find(Bio).length, 1)
+  })
+})

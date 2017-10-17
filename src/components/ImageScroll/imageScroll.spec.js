beb7a0d5ba5f5580dd5e2abd8de7bf7e8661ae0c
Donovan <donovan6925@gmail.com>
2017-10-16 22:18:19 -0400

    added ImageScroll tests

---
+++ src/components/ImageScroll/imageScroll.spec.js
@@ -0,0 +1,19 @@
+/*global shallow should*/
+/*eslint no-undef: ["error", { "typeof": true }] */
+
+import React from 'react'
+
+import ImageScroll from './index'
+
+describe('ImageScroll', () => {
+  const defaultProps = {
+    images: []
+  }
+
+  const wrapper = shallow(<ImageScroll {...defaultProps} />)
+
+  it('successfully renders', () => {
+    should.equal(wrapper.length, 1)
+  })
+
+})
